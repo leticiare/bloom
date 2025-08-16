119002ebdbbbbc0da8f209c606743f1c2f491a61
leticiare <leticia.rezende2010@outlook.com>
2025-08-16 16:15:05 -0300

    refactor: muda tempo de expiração de token para variavel de ambiente

--- bloom-api/auth/AuthService.py
+++ bloom-api/auth/AuthService.py
@@ -15,6 +15,7 @@ class ServicoAutenticacao:
     oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
     SECRET_KEY = os.getenv("SECRET_KEY")
     ALGORITHM = os.getenv("ALGORITHM")
+    ACCESS_EXPIRES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES") or 30
 
     @classmethod
     def _verificar_senha(cls, senha_entrada: str, hash_senha: str) -> bool:
@@ -25,7 +26,11 @@ class ServicoAutenticacao:
         return cls.pwd_context.hash(senha)
 
     @classmethod
-    def _gerar_token_acesso(cls, data: dict, expires_delta: Optional[timedelta] = None):
+    def _gerar_token_acesso(
+        cls,
+        data: dict,
+        expires_delta: Optional[timedelta] = timedelta(int(ACCESS_EXPIRES)),
+    ):
         to_encode = data.copy()
         if expires_delta:
             expire = datetime.utcnow() + expires_delta
@@ -54,16 +59,14 @@ class ServicoAutenticacao:
         usuario_encontrado: Usuario = (
             await RepositorioUsuario().buscar_usuario_por_email(usuario.email)
         )
-        logger.debug(usuario.senha)
 
-        logger.debug(f"encontrado: {usuario_encontrado}")
         if not usuario_encontrado or not cls._verificar_senha(
             usuario.senha, usuario_encontrado.senha
         ):
             raise ValueError("Usuário ou senha incorretos")
 
         dados_usuario = {"email": usuario.email, "perfil": usuario.perfil}
-        token = cls._gerar_token_acesso(dados_usuario, timedelta(minutes=30))
+        token = cls._gerar_token_acesso(dados_usuario)
         return {"jwt_token": token}
 
 
