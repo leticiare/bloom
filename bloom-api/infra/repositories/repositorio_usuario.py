f81875160cedd3ce68d4265bbef7da0477294977
Davi Bittencourt <dbittencourt2002@gmail.com>
2025-08-23 12:35:23 -0300

    fix: corrige uso da classe de conexão do banco para interpolar valores SQL

--- bloom-api/infra/repositories/repositorio_usuario.py
+++ bloom-api/infra/repositories/repositorio_usuario.py
@@ -1,11 +1,11 @@
-import os
-
 from domain.entities.entidade_usuario import Usuario
 from dotenv import load_dotenv
 
 from infra.db.conexao import ConexaoBancoDados
 from infra.db.iniciar_db import conexao
 from infra.logger.logger import logger
+
+from psycopg2.sql import SQL, Identifier
 
 load_dotenv()
 
@@ -14,12 +14,14 @@ class RepositorioUsuario:
     def __init__(self):
         self._conexao: ConexaoBancoDados = ConexaoBancoDados.obter_instancia()
         self._tabela: str = "usuario"
-        self._schema: str = os.getenv("SCHEMA_DB")
 
     async def buscar_usuario_por_email(self, email: str) -> Usuario:
         try:
+            sql = SQL("""
+                    SELECT * FROM {tabela} WHERE email = %s
+                """).format(tabela=Identifier(self._tabela))
             resultado = conexao.executar_sql(
-                f"SELECT * FROM bloom_db.{self._tabela} WHERE email = '{email}'", True
+                sql=sql, parametros=(email,), possui_resultado=True
             )[0]
             logger.debug(f"BUSCA: {resultado}")
             if not resultado:
@@ -30,18 +32,20 @@ class RepositorioUsuario:
             )
             return usuario
         except Exception as e:
-            logger.info(f"Erro ao reciperar usuario: {e}")
+            logger.error(f"Erro ao recuperar usuario: {e}")
             raise e
 
     async def inserir_usuario(self, usuario: Usuario):
         try:
-            query = f"""
-            INSERT INTO bloom_db.{self._tabela} (email, senha, perfil)
-            VALUES ('{usuario.email}', '{usuario.senha}', '{usuario.perfil}')
-            """
-            self._conexao.executar_sql(query)
+            sql = SQL("""
+                  INSERT INTO {tabela} (email, senha, perfil) VALUES (%s, %s, %s)
+                """).format(tabela=Identifier(self._tabela))
+
+            self._conexao.executar_sql(
+                sql=sql, parametros=(usuario.email, usuario.senha, usuario.perfil)
+            )
         except Exception as e:
-            logger.info(f"Erro ao cadastrar usuario: {e}")
+            logger.error(f"Erro ao cadastrar usuario: {e}")
             raise e
 
 
