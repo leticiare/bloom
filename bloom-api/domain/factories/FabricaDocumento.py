209dbb10ed7e802dd37a0bfe2c9cdde3c645b678
leticiare <leticia.rezende2010@outlook.com>
2025-08-24 23:57:04 -0300

    feat: cria contrato, documentos e fábrica para os documentos

---
+++ bloom-api/domain/factories/FabricaDocumento.py
@@ -0,0 +1,16 @@
+from domain.contracts.Documento import Documento
+from domain.entities.Documentos import CNPJ, CPF
+from domain.enums.TiposDocumento import TiposDocumento
+
+
+class FabricaDocumento:
+    def criar_documento(tipo: TiposDocumento, numero: str) -> Documento:
+        tipo_map = {"cpf": CPF, "cnpj": CNPJ}
+        classe_documento = tipo_map.get(tipo.lower())
+
+        documento_obj = classe_documento(numero)
+
+        if not documento_obj.validar():
+            raise ValueError(f"Número de {tipo.upper()} inválido.")
+
+        return documento_obj
