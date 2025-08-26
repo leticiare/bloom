30141dfb3f4288348382975108d37a579439dfdf
leticiare <leticia.rezende2010@outlook.com>
2025-08-25 21:29:14 -0300

    feat: cria DTOs para gestante e profissional

---
+++ bloom-api/controllers/dto/ProfissionalDto.py
@@ -0,0 +1,24 @@
+from domain.entities.Profissional import Profissional
+from domain.enums.EspecialidadesMedicas import EspecialidadesMedicas
+from domain.enums.UsuarioPerfil import UsuarioPerfil
+
+from .UsuarioDto import UsuarioDTO
+
+
+class ProfissionalDTO(UsuarioDTO):
+    perfil: UsuarioPerfil = UsuarioPerfil.PROFISSIONAL
+    nome: str
+    especialidade: EspecialidadesMedicas
+    registro: str
+
+    def para_entidade(self) -> Profissional:
+        return Profissional(
+            email=self.email,
+            senha=self.senha,
+            documento=self.documento,
+            tipo_documento=self.tipo_documento,
+            data_nascimento=self.data_nascimento,
+            nome=self.nome,
+            especialidade=self.especialidade,
+            registro=self.registro,
+        )
