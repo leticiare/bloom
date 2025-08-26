da2819fc2ff0216988b144aaec03c404ba62cf10
leticiare <leticia.rezende2010@outlook.com>
2025-08-25 21:30:13 -0300

    refactor: completa informações da entidade usuário

--- bloom-api/domain/entities/entidade_usuario.py
+++ bloom-api/domain/entities/entidade_usuario.py
@@ -1,7 +1,11 @@
 from dataclasses import dataclass
+from datetime import datetime
 from typing import Literal
 
 from pydantic import EmailStr
+
+from domain.contracts.Documento import Documento
+from domain.enums.TiposDocumento import TiposDocumento
 
 
 @dataclass
@@ -9,3 +13,6 @@ class Usuario:
     email: EmailStr
     senha: str
     perfil: Literal["gestante", "profissional"]
+    documento: Documento
+    tipo_documento: TiposDocumento
+    data_nascimento: datetime
