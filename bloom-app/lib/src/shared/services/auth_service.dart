298078e63e731be8cc7f6b74008daf26327e9735
danieljstri <euft6y@gmail.com>
2025-08-31 22:58:08 -0300

    feat: finalmente implementado e testado o login

--- bloom-app/lib/src/shared/services/auth_service.dart
+++ bloom-app/lib/src/shared/services/auth_service.dart
@@ -1,27 +1,30 @@
-import 'dart:convert';
+import 'dart:convert'; // Importe o dart:convert para usar o json.encode
 import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class AuthService {
   // A URL base da sua API. Use o IP 10.0.2.2 para o emulador Android
   // se a API estiver rodando localmente na sua máquina.
-  final String _baseUrl = "http://10.0.2.2:8000/api/v1";
+  final String _baseUrl = "http://localhost:8000/api";
   final _storage = const FlutterSecureStorage();
 
   // Função para realizar o login
   Future<String?> login(String email, String password) async {
     final response = await http.post(
-      Uri.parse('$_baseUrl/auth/login'),
+      Uri.parse('$_baseUrl/auth/login'), // <-- CORREÇÃO 1: A rota é /token
+      // CORREÇÃO 2: Mudar o cabeçalho
       headers: <String, String>{
-        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Type': 'application/json; charset=UTF-8',
       },
-      // A API espera dados de formulário, não JSON
-      body: {'username': email, 'password': password},
+
+      // CORREÇÃO 3: Enviar como mapa, não como JSON
+      body: json.encode(<String, String>{'email': email, 'senha': password}),
     );
 
     if (response.statusCode == 200) {
       final Map<String, dynamic> responseData = json.decode(response.body);
-      final String accessToken = responseData['access_token'];
+      final Map<String, dynamic> dados = responseData['dados'];
+      final String accessToken = dados['jwt_token'];
 
       // Armazena o token de forma segura
       await _storage.write(key: 'access_token', value: accessToken);
@@ -31,7 +34,7 @@ class AuthService {
       // Tenta decodificar a mensagem de erro da API
       try {
         final Map<String, dynamic> errorData = json.decode(response.body);
-        return errorData['detail'] ?? 'Erro desconhecido ao fazer login.';
+        return errorData['mensagem'] ?? 'Erro desconhecido ao fazer login.';
       } catch (e) {
         return 'Erro ao processar a resposta do servidor.';
       }
