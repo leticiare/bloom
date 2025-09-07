4ad7ea0c8b2bd18a28396f738d917467925f44d6
leticiare <leticia.rezende2010@outlook.com>
2025-09-07 02:50:48 -0300

    feat: cria controlador e classes para artigo

--- bloom-api/domain/entities/Artigo.py
+++ bloom-api/domain/entities/Artigo.py
@@ -3,10 +3,19 @@ from dataclasses import dataclass
 
 @dataclass
 class Artigo:
-    def __init__(self, titulo: str, temas: list[str], conteudo: str):
+    def __init__(
+        self,
+        id: str,
+        titulo: str,
+        temas: list[str],
+        conteudo: str,
+        autor: str,
+    ):
+        self.id = id
         self.titulo = titulo
-        self.temas = temas
+        self.temas = temas if isinstance(temas, list) else [temas]
         self.conteudo = conteudo
+        self.autor = autor
 
     @property
     def titulo_contem_texto(self):
@@ -21,3 +30,12 @@ class Artigo:
         if len(self.temas) > 0:
             return True
         return False
+
+    def para_dicionario(self):
+        return {
+            "id": self.id,
+            "titulo": self.titulo,
+            "temas": self.temas,
+            "conteudo": self.conteudo,
+            "autor": self.autor,
+        }
